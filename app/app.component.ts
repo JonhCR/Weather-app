8b534b83467c0b29ba9aa2afc1d171208071f476
Jhon Moreira <jonh.m.10@gmail.com>
2016-04-21 10:54:18 -0600

    Add two way data binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,15 +3,53 @@ import {Weather} from './weather';
 
 @Component({
     selector: 'my-app',
-    template: ' <ul> <li>Clima tipo : {{ weather.main}}</li> <li> Clima descripción : {{ weather.description }}</li>  </ul>'
+    template: '<input [(ngModel)] = "city" placeholder="Busca alguna ciudad" (keyup)="addCity(city , $event)"> ' +
+    '<h2>Este es el clima en la ciudad : {{city}}</h2>' +
+    '<ul *ngFor="#weather of weatherOfCities"> <li>' +
+    '<h2>{{weather.city}}</h2>' +
+    '<ul>' +
+    '<li>Clima : {{weather.main}}</li><li>Descripción : {{weather.description}}</li>' +
+    '</ul>' +
+    '</li> </ul>'
 })
 export class AppComponent {
 
-  city :string;
-  weather:Weather;
+  public city:string ;
+  public cities: Array<string>;
+  public weatherOfCities: Array<Weather>;
 
   constructor(){
-   this.weather = new Weather(1 ,"San josé" , "muy nublado" , "nublado");
+    this.city = "";
+    this.weatherOfCities = [];
   }
 
+  getWeather = function (city:string){
+      var weather:Weather;
+      if(city.toLocaleLowerCase() == "viena"){
+          weather = {
+            "id" : 1,
+            "city" : "viena",
+            "main" : "nublado",
+            "description" : "Bastante nublado"
+          }
+        }else if (city.toLocaleLowerCase() == "london"){
+          weather = {
+            "id" : 2,
+            "city" : "london",
+            "main" : "lloviendo",
+            "description" : "Lluvias torrenciales"
+          }
+        }
+    return weather;
+  }
+
+  addCity = function (city:string , $event){
+        if($event.keyCode == 13){
+            var weather = this.getWeather(city);
+            if(weather){
+              this.weatherOfCities.push(this.getWeather(city));
+            }
+            this.city = "";
+        }
+  }
 }
