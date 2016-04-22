fda35a9f826db3146dc13ef31f7d6a6dfc70dc51
Jhon Moreira <jonh.m.10@gmail.com>
2016-04-22 08:24:58 -0600

    Add style to weather component

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,14 +3,40 @@ import {Weather} from './weather';
 
 @Component({
     selector: 'my-app',
-    template: '<input [(ngModel)] = "city" placeholder="Busca alguna ciudad" (keyup)="addCity(city , $event)"> ' +
-    '<h2>Este es el clima en la ciudad : {{city}}</h2>' +
-    '<ul *ngFor="#weather of weatherOfCities"> <li>' +
-    '<h2>{{weather.city}}</h2>' +
-    '<ul>' +
-    '<li>Clima : {{weather.main}}</li><li>Descripción : {{weather.description}}</li>' +
-    '</ul>' +
-    '</li> </ul>'
+    template: `<header><h1>Clima App</h1></header>
+    <div class="content">
+      <input [(ngModel)] = "city" placeholder="Busca alguna ciudad" (keyup)="addCity(city , $event)"> 
+      <h2>Este es el clima en la ciudad : {{city}}</h2>
+      <ul *ngFor="#weather of weatherOfCities" class="weather-card"> 
+        <li>
+          <h2>{{weather.city}}</h2>
+          <ul>
+            <li>Clima : {{weather.main}}</li>
+            <li>Descripción : {{weather.description}}</li>
+          </ul>
+        </li> 
+      </ul>
+    </div> ` ,
+    styles:[` 
+      header h1 {
+      padding: 10px;
+      background: #5E9EA0;
+      color: #F5F5F5;
+      text-shadow: 1px 1px 4px #808080;
+      }    
+      .content{
+      padding: 10px;
+      }
+      input{
+      font-size: 10px;
+      padding: 4px;
+      }
+      .weather-card{
+      border-bottom: 1px solid #03D3D3;
+      padding-bottom: 10px;
+      }
+    ` ]
+
 })
 export class AppComponent {
 
